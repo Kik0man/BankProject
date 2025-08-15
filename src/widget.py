b7c8c7eef13a37f4ea59bb7ce631e5ea1b26508c
Kikoman <yanhok2001@gmail.com>
2025-08-15 22:48:20 +0500

    widget_second_version

--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,15 @@
-from mask import get_mask_account
-
-from mask import get_mask_card_number
+from mask import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(full_bank_info: str) -> str:
     """Маскирует номер карты или счета в зависимости от типа.
-    Формат ввода: 'Тип Номер' """
+    Формат ввода: 'Тип Номер'"""
 
     # Разделяем строку на тип и номер по пробелам
     words = full_bank_info.split()
 
     # Присваиваем значения после разделения
-    account_type = ' '.join(words[:-1])
+    account_type = " ".join(words[:-1])
     number = words[-1]
 
     # Определяем тип и применяем соответствующую маскировку
