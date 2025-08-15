dbf455bad06bc7bc30c772683e4e949f3ba89063
Kikoman <yanhok2001@gmail.com>
2025-08-15 22:42:37 +0500

    widget_second_version

---
+++ src/widget.py
@@ -0,0 +1,21 @@
+from mask import get_mask_account
+
+from mask import get_mask_card_number
+
+
+def mask_account_card(full_bank_info: str) -> str:
+    """Маскирует номер карты или счета в зависимости от типа.
+    Формат ввода: 'Тип Номер' """
+
+    # Разделяем строку на тип и номер по пробелам
+    words = full_bank_info.split()
+
+    # Присваиваем значения после разделения
+    account_type = ' '.join(words[:-1])
+    number = words[-1]
+
+    # Определяем тип и применяем соответствующую маскировку
+    if "счет" in account_type.lower():
+        return f"{account_type} {get_mask_account(number)}"
+    else:
+        return f"{account_type} {get_mask_card_number(number)}"
