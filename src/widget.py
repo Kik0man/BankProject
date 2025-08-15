43d847ac8dd1aa0ae59519493a678fc5d53ca1fa
Kikoman <yanhok2001@gmail.com>
2025-08-15 22:58:46 +0500

    widget_add_get_date

--- src/widget.py
+++ src/widget.py
@@ -17,3 +17,20 @@ def mask_account_card(full_bank_info: str) -> str:
         return f"{account_type} {get_mask_account(number)}"
     else:
         return f"{account_type} {get_mask_card_number(number)}"
+
+
+def get_date(date_str: str) -> str:
+    """Преобразует дату из формата "ГГГГ-ММ-ДДTЧЧ:ММ:СС.микросекунды" в "ДД.ММ.ГГГГ" """
+    # 1. Разделяем дату и время
+    date_time_parts = date_str.split("T")
+    date_part = date_time_parts[0]
+
+    # 2. Разбиваем дату на компоненты
+    date_components = date_part.split("-")
+
+    # 3. Извлекаем отдельные компоненты
+    year = date_components[0]
+    month = date_components[1]
+    day = date_components[2]
+
+    return f"{day}.{month}.{year}"
