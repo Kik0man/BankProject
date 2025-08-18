71c21373133c4e207b54b7513dc35dfa023af256
Kikoman <yanhok2001@gmail.com>
2025-08-18 18:26:28 +0500

    new_feature

---
+++ src/processing.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+
+def filter_by_state(transactions: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
+    """Фильтрует список словарей по значению ключа 'state'."""
+    result = []
+    for word in transactions:
+        if word.get("state") == state:
+            result.append(word)
+    return result
+
+
+def sort_by_date(transactions: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+    """Сортирует список словарей по дате, в зависимости от значения reverse"""
+    return sorted(
+        transactions,
+        key=lambda x: x["date"],
+        reverse=reverse,
+    )
